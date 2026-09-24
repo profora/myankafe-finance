faf103f9872f50f4329ca9c607ad31e19aa02f3f
Kyaw Thant Tin <kyawthanttin@gmail.com>
2026-09-24 22:51:13 +0630

    feat: register owner password reset route

--- backend/internal/httpapi/server.go
+++ backend/internal/httpapi/server.go
@@ -71,6 +71,7 @@ func New(store *postgres.Store, cfg config.Config) http.Handler {
 			r.Post("/entities", s.createEntity)
 			r.Get("/users", s.listUsers)
 			r.Post("/users", s.createUser)
+			r.Post("/users/{user}/reset-password", s.resetUserPassword)
 			r.Post("/inter-entity-transactions", s.createInterEntityExpense)
 			r.Get("/dashboard/combined", s.combinedDashboard)
 
