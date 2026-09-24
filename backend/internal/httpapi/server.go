f64564f6be2d867d8f3c4955fed457506af90494
Kyaw Thant Tin <kyawthanttin@gmail.com>
2026-09-25 00:30:56 +0630

    feat: register user status administration route

--- backend/internal/httpapi/server.go
+++ backend/internal/httpapi/server.go
@@ -76,6 +76,8 @@ func New(store *postgres.Store, cfg config.Config) http.Handler {
 			r.Get("/users", s.listUsers)
 			r.Post("/users", s.createUser)
 			r.Post("/users/{user}/reset-password", s.resetUserPassword)
+			r.Put("/users/{user}/status", s.setUserStatus)
+			r.Post("/users/{user}/reset-password", s.resetUserPassword)
 			r.Post("/inter-entity-transactions", s.createInterEntityExpense)
 			r.Get("/dashboard/combined", s.combinedDashboard)
 
