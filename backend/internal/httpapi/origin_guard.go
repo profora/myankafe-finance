0c88c58945453c98069ec9021b51d1f148c44256
Kyaw Thant Tin <kyawthanttin@gmail.com>
2026-09-24 23:20:45 +0630

    security: reject cross-origin browser mutations

---
+++ backend/internal/httpapi/origin_guard.go
@@ -0,0 +1,21 @@
+package httpapi
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+func (s *Server) originGuard(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request){
+		switch r.Method{
+		case http.MethodPost,http.MethodPut,http.MethodPatch,http.MethodDelete:
+			origin:=strings.TrimSpace(r.Header.Get("Origin"))
+			if origin!=""&&origin!=s.Config.CORSOrigin{
+				fail(w,http.StatusForbidden,errors.New("origin not allowed"))
+				return
+			}
+		}
+		next.ServeHTTP(w,r)
+	})
+}
