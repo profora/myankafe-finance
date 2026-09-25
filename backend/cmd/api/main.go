cfedccf2fd75683330227b7b5f255b18da828e79
Kyaw Thant Tin <kyawthanttin@gmail.com>
2026-09-25 11:08:53 +0630

    ops: emit structured API lifecycle logs

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -15,6 +16,8 @@ import (
 )
 
 func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})))
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
@@ -32,9 +35,12 @@ func main() {
 		ReadHeaderTimeout: 10 * time.Second,
 	}
 
+	slog.Info("api_starting", "addr", cfg.HTTPAddr, "app_env", cfg.AppEnv, "auth_mode", cfg.AuthMode)
+
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal(err)
+			slog.Error("api_listen_failed", "error", err)
+			os.Exit(1)
 		}
 	}()
 
@@ -42,7 +48,13 @@ func main() {
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
 	<-stop
 
+	slog.Info("api_shutdown_requested")
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_ = srv.Shutdown(ctx)
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Error("api_shutdown_failed", "error", err)
+		return
+	}
+	slog.Info("api_stopped")
 }
